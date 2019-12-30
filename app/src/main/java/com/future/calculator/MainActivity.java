aff4ed8c57e1effe438d649e308fb5143ffb3970
ohowen <hgyouco@gmail.com>
2019-12-30 14:08:37 +0800

    退出保存数据

--- app/src/main/java/com/future/calculator/MainActivity.java
+++ app/src/main/java/com/future/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.future.calculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -12,16 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Context context;
     private EditText et_x1, et_x2, et_y1, et_y2;
     private TextView tv_result;
 
-    private float x1, x2, y1, y2;
+    private float x1, x2, y1, y2, result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        context = this;
+        initValues();
         initView();
         initListener();
     }
@@ -32,6 +35,32 @@ public class MainActivity extends AppCompatActivity {
         et_y1 = findViewById(R.id.et_y1);
         et_y2 = findViewById(R.id.et_y2);
         tv_result = findViewById(R.id.tv_result);
+
+        if (x1 > 0) {
+            et_x1.setText(String.valueOf(x1));
+        }
+
+        if (x2 > 0) {
+            et_x2.setText(String.valueOf(x2));
+        }
+
+        if (y1 > 0) {
+            et_y1.setText(String.valueOf(y1));
+        }
+
+        if (y2 > 0) {
+            et_y2.setText(String.valueOf(y2));
+        }
+
+        tv_result.setText(String.format(getResources().getString(R.string.result), result));
+    }
+
+    private void initValues() {
+        x1 = SharePreUtil.getInstance().getValue(context, SharePreUtil.KEY_X1);
+        x2 = SharePreUtil.getInstance().getValue(context, SharePreUtil.KEY_X2);
+        y1 = SharePreUtil.getInstance().getValue(context, SharePreUtil.KEY_Y1);
+        y2 = SharePreUtil.getInstance().getValue(context, SharePreUtil.KEY_Y2);
+        result = SharePreUtil.getInstance().getValue(context, SharePreUtil.KEY_RESULT);
     }
 
     private void initListener() {
@@ -128,14 +157,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     private void calculate() {
         if (x1 > 0 && x2 > 0 && y1 > 0 && y2 > 0) {
-            float result = (x1 * y1 + x2 * y2) / (x2 + x1);
+            result = (x1 * y1 + x2 * y2) / (x2 + x1);
             Log.d("calculate", "RESULT: " + result);
             tv_result.setText(String.format(getResources().getString(R.string.result), result));
         } else {
             tv_result.setText(String.format(getResources().getString(R.string.result), 0.00f));
         }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharePreUtil.getInstance().saveValue(context, SharePreUtil.KEY_X1, x1);
+        SharePreUtil.getInstance().saveValue(context, SharePreUtil.KEY_X2, x2);
+        SharePreUtil.getInstance().saveValue(context, SharePreUtil.KEY_Y1, y1);
+        SharePreUtil.getInstance().saveValue(context, SharePreUtil.KEY_Y2, y2);
+        SharePreUtil.getInstance().saveValue(context, SharePreUtil.KEY_RESULT, result);
     }
 }
---
+++ app/src/main/java/com/future/calculator/SharePreUtil.java
@@ -0,0 +1,47 @@
+package com.future.calculator;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * ProjectName: Future
+ * Function: 记录.
+ * Date: 2019-12-27 18:07
+ * <p>
+ * Created by jiangrh@solutionexp.com
+ */
+class SharePreUtil {
+
+    private static final String SHARE_PRE = "value_info";
+    static final String KEY_X1 = "X1";
+    static final String KEY_X2 = "X2";
+    static final String KEY_Y1 = "Y1";
+    static final String KEY_Y2 = "Y2";
+    static final String KEY_RESULT = "RESULT";
+
+    private volatile static SharePreUtil instance;
+
+    private SharePreUtil() {
+    }
+
+    static SharePreUtil getInstance() {
+        if (instance == null) {
+            synchronized (SharePreUtil.class) {
+                if (instance == null) {
+                    instance = new SharePreUtil();
+                }
+            }
+        }
+        return instance;
+    }
+
+    void saveValue(Context context, String key, float value) {
+        SharedPreferences sp = context.getSharedPreferences(SHARE_PRE, Context.MODE_PRIVATE);
+        sp.edit().putFloat(key, value).apply();
+    }
+
+    float getValue(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(SHARE_PRE, Context.MODE_PRIVATE);
+        return sp.getFloat(key, 0F);
+    }
+}
